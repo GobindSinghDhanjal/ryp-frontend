e0c4bc14d4e51fe7fc2a17a616ed1d072e5440b5
Gobind <gobindsinghdhanjal1@gmail.com>
2025-01-12 10:13:41 +0530

    resolved some errors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,25 @@
-module.exports = {
-    experimental: {
-      missingSuspenseWithCSRBailout: false,
-    },
-  }
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     serverComponentsHmrCache: false, // defaults to true
+//   },
+// };
+
+// module.exports = nextConfig;
+
+// module.exports = {
+//   experimental: {
+//     missingSuspenseWithCSRBailout: false,
+//   },
+// };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsHmrCache: false, // defaults to true
+    missingSuspenseWithCSRBailout: false, // Add both experimental options
+  },
+};
+
+module.exports = nextConfig;
+
