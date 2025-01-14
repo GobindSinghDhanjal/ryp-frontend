fdb0fb5a9b868f997a596aa4337916346be00631
Gobind <gobindsinghdhanjal1@gmail.com>
2025-01-14 16:17:42 +0530

    resolved some errors

--- app/notifications/loading.js
+++ app/notifications/loading.js
@@ -1,20 +1,22 @@
-// /components/Loading.js
 import { Skeleton, Box } from "@mui/material";
 
 export default function Loading() {
   return (
-    <Box
-      sx={{ display: "flex", flexDirection: "column", gap: 4, maxWidth: 400 }}
-    >
-      {[...Array(2)].map((_, index) => (
-        <Skeleton
-          key={index}
-          sx={{ borderRadius: 2 }}
-          variant="rectangular"
-          width="100%"
-          height={100}
-        />
-      ))}
-    </Box>
+    <div className="container sub-container">
+    <h1>Notifications</h1>
+      <Box
+        sx={{ display: "flex", flexDirection: "column", gap: 4, maxWidth: 400 }}
+      >
+        {[...Array(2)].map((_, index) => (
+          <Skeleton
+            key={index}
+            sx={{ borderRadius: 2 }}
+            variant="rectangular"
+            width="100%"
+            height={160}
+          />
+        ))}
+      </Box>
+    </div>
   );
 }
--- app/notifications/page.js
+++ app/notifications/page.js
@@ -1,14 +1,113 @@
-import NotificationComponent from "@/app/components/NotificationComponent";
+"use client";
+import { useEffect, useState } from "react";
+import { Card, CardContent, Typography, Box } from "@mui/material";
 import Loading from "./loading";
-import { Suspense } from "react";
 
-export default function NotificationsPage() {
+const NotificationComponent = () => {
+  const [notifications, setNotifications] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true); // Loading state
+
+  useEffect(() => {
+    const fetchNotifications = async () => {
+      try {
+        const token = process.env.NEXT_PUBLIC_PASSCODE || "";
+
+        const response = await fetch("/api/notifications", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          cache: "force-cache",
+        });
+
+        if (!response.ok) {
+          throw new Error("Authentication failed or other error.");
+        }
+
+        const data = await response.json();
+
+        if (data.length) {
+          setNotifications(data);
+        }
+      } catch (error) {
+        console.error("Error fetching notifications:", error);
+        setError("Failed to load notifications.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNotifications();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
   return (
     <div className="container sub-container">
-      <h2>Notifications</h2>
-      <Suspense fallback={<Loading />}>
-        <NotificationComponent />
-      </Suspense>
+      <h1>Notifications</h1>
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 4,
+        }}
+      >
+        {notifications.map((notification) => (
+          <Card
+            key={notification._id}
+            sx={{
+              maxWidth: 400,
+              width: "100%",
+              borderRadius: 2,
+              boxShadow: "2px 2px 8px rgba(0, 0, 0, 0.21)",
+            }}
+          >
+            <CardContent sx={{ minHeight: 100, position: "relative" }}>
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ marginBottom: 1, fontSize: "1rem" }}
+              >
+                {notification.title}
+              </Typography>
+              <Typography
+                variant="body1"
+                component="div"
+                sx={{ marginBottom: 1, fontSize: "0.9rem" }}
+              >
+                {notification.message}
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{
+                  position: "absolute",
+                  bottom: 8,
+                  right: 8,
+                  fontSize: "0.75rem", // Smaller font size for the date
+                }}
+              >
+                {new Date(notification.date).toLocaleDateString("en-GB", {
+                  day: "2-digit",
+                  month: "short",
+                  year: "numeric",
+                })}
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+      </Box>
     </div>
   );
-}
+};
+
+export default NotificationComponent;
