3b4d6ca567bd34f3e8a3b7ce846724ebe431f863
Gobind <gobindsinghdhanjal1@gmail.com>
2025-02-01 10:45:32 +0530

    fixed some issues

--- app/api/professors/tempProfessor/all/route.js
+++ app/api/professors/tempProfessor/all/route.js
@@ -2,12 +2,28 @@ import { NextResponse } from "next/server";
 import dbConnect from "@/app/utils/dbConnect";
 import TempProfessor from "@/app/models/TempProfessor";
 
-// GET all temporary professors
+// // GET all temporary professors
+// export async function GET() {
+//   try {
+//     await dbConnect();
+//     const tempProfessors = await TempProfessor.find();
+//     return NextResponse.json(tempProfessors);
+//   } catch (err) {
+//     console.error(err.message);
+//     return NextResponse.json({ error: "Server Error" }, { status: 500 });
+//   }
+// }
+
 export async function GET() {
   try {
     await dbConnect();
     const tempProfessors = await TempProfessor.find();
-    return NextResponse.json(tempProfessors);
+
+    return NextResponse.json(tempProfessors, {
+      headers: {
+        "Cache-Control": "no-store",
+      },
+    });
   } catch (err) {
     console.error(err.message);
     return NextResponse.json({ error: "Server Error" }, { status: 500 });
