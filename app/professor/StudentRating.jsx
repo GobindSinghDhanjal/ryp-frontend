98e654bae755b1f501029823e0cc1d9989a21b53
Gobind <gobindsinghdhanjal1@gmail.com>
2025-04-13 12:53:02 +0530

    updated the ui of some components

--- app/professor/StudentRating.jsx
+++ app/professor/StudentRating.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import { Box, Typography, Rating, Paper, Divider, Button } from '@mui/material';
+import React, { useState } from "react";
+import { Box, Typography, Rating, Paper, Divider, Button } from "@mui/material";
 
 const StudentRating = ({ feedback }) => {
   const [visibleFeedbacks, setVisibleFeedbacks] = useState(4);
 
   const loadMoreFeedbacks = () => {
-    setVisibleFeedbacks(prev => prev + 4);
+    setVisibleFeedbacks((prev) => prev + 4);
   };
 
   if (!feedback || feedback.length === 0) {
     return (
-      <Box sx={{ textAlign: 'center', my: 2 }}>
+      <Box sx={{ textAlign: "center", my: 2 }}>
         <Typography variant="body1">No ratings available</Typography>
       </Box>
     );
@@ -23,27 +23,45 @@ const StudentRating = ({ feedback }) => {
   return (
     <>
       {sortedFeedbacks.map(({ rating, comment, date }, index) => (
-        <Paper key={index} elevation={3} sx={{ padding: 2, marginBottom: 3 }}>
+        <Paper
+          key={index}
+          elevation={3}
+          sx={{
+            padding: 2,
+            marginBottom: 3,
+            borderRadius: 2,
+            boxShadow: "1px 1px 8px rgba(0, 0, 0, 0.09)",
+          }}
+        >
           <Box>
             <Typography variant="h6">Student</Typography>
             <Rating name="read-only" precision={0.5} value={rating} readOnly />
           </Box>
           <Typography variant="body1">{comment}</Typography>
           {/* <Divider sx={{ margin: '8px 0' }} /> */}
-          <Box sx={{ display: 'block',textAlign:"right", justifyContent: 'space-between', marginTop: 1 }}>
+          <Box
+            sx={{
+              display: "block",
+              textAlign: "right",
+              justifyContent: "space-between",
+              marginTop: 1,
+            }}
+          >
             <Typography variant="body2" color="textSecondary">
-              {new Date(date).toLocaleDateString('en-IN', {
-                month: 'short',
-                year: 'numeric',
-                timeZone: 'Asia/Kolkata'
+              {new Date(date).toLocaleDateString("en-IN", {
+                month: "short",
+                year: "numeric",
+                timeZone: "Asia/Kolkata",
               })}
             </Typography>
           </Box>
         </Paper>
       ))}
       {feedback.length > visibleFeedbacks && (
-        <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 2 }}>
-          <Button variant="contained" onClick={loadMoreFeedbacks}>Load More</Button>
+        <Box sx={{ display: "flex", justifyContent: "center", marginTop: 2 }}>
+          <Button variant="contained" onClick={loadMoreFeedbacks}>
+            Load More
+          </Button>
         </Box>
       )}
     </>
